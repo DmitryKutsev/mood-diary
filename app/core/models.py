c96b17f201674241fccfc9b26c2742f824ba7787
dima_in_the_forest <dmitrykucev@mail.ru>
2023-02-02 22:42:06 +0100

    Added functions for creating volunteer and superuser.

--- app/core/models.py
+++ app/core/models.py
@@ -10,11 +10,31 @@ class UserManager(BaseUserManager):
     """Manager for users."""
 
     def create_user(self, email, password=None, **extra_fields):
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('User must have an email address.')
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
+
+    def create_volunteer(self, email, password=None):
+
+        user = self.create_user(email, password)
+        user.is_volunteer = True
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None):
+
+        user = self.create_volunteer(email, password)
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -22,7 +42,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    is_volunteer = models.BooleanField(default=True)
+    is_volunteer = models.BooleanField(default=False)
 
     objects = UserManager()
 
--- app/core/tets/test_models.py
+++ app/core/tets/test_models.py
@@ -18,3 +18,45 @@ class ModelTests(TestCase):
         )
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test if email is normalized for new users."""
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['test2@EXAMPLE.COM', 'test2@example.com'],
+        ]
+
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample12')
+            self.assertEqual(user.email, expected)
+            self.assertFalse(user.is_superuser)
+            self.assertFalse(user.is_volunteer)
+
+    def test_create_volunteer(self):
+        """Test creating a superuser."""
+
+        email = 'test@example.com'
+        password = 'testpass123'
+        user = get_user_model().objects.create_volunteer(
+            email=email,
+            password=password,
+        )
+        self.assertFalse(user.is_superuser)
+        self.assertTrue(user.is_volunteer)
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+
+    def test_create_superuser(self):
+        """Test creating a superuser."""
+
+        email = 'test@example.com'
+        password = 'testpass123'
+        user = get_user_model().objects.create_superuser(
+            email=email,
+            password=password,
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_volunteer)
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
