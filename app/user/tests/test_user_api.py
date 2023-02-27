bb7ec986a2faf9e3ceda96bbfbc48919be5a2d7c
dima_in_the_forest <dmitrykucev@mail.ru>
2023-02-27 12:34:55 +0100

    Added user app.

---
+++ app/user/tests/test_user_api.py
@@ -0,0 +1,31 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import TestCase
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+CREATE_USER_URL = reverse('user:create')
+
+def create_user(**params):
+    """Create and return a new user."""
+    return get_user_model().object.create_user(**params)
+
+class PublicUserApiTests(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_user_success(self):
+        """Test creating a user is successful"""
+        payload = {
+            'email': 'test@example.com',
+            'password': 'testpas123',
+            'name': 'Test Name',
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        user = get_user_model().objects.get(email=payload['email'])
+        self.assertTrue(user.check_password(payload['password']))
+        
