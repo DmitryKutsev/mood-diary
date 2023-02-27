df90f917cd299ba8ff3df09b041b5af62a6d5a8f
dima_in_the_forest <dmitrykucev@mail.ru>
2023-02-27 13:14:03 +0100

    Added urls and view for user app

--- app/user/views.py
+++ app/user/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user in a system"""
+    serializer_class = UserSerializer
