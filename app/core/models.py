cdc1860d414aa46b0169d5246a7edc4ad4dcb972
dima_in_the_forest <dmitrykucev@mail.ru>
2023-03-31 21:15:36 +0200

    Removed "is_volunteer" migrations

--- app/core/models.py
+++ app/core/models.py
@@ -19,19 +19,30 @@ class UserManager(BaseUserManager):
 
         return user
 
-    def create_volunteer(self, email, password=None):
-        user = self.create_user(email, password)
-        user.is_staff = True
-        user.save(using=self._db)
-
-        return user
-
     def create_superuser(self, email, password=None):
         user = self.create_volunteer(email, password)
         user.is_superuser = True
         user.save(using=self._db)
 
         return user
+
+    def create_therapist(self, email, password=None):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
+class Therapist(AbstractBaseUser, PermissionsMixin, models.Model):
+    """User in the system"""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -42,4 +53,23 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     objects = UserManager()
+    therapist = models.ManyToManyField(Therapist, verbose_name='theraist')
     USERNAME_FIELD = 'email'
+
+
+class Emotions(models.Model):
+    """Emotions listed in the entry"""
+
+    name = models.CharField('Emotions name', max_length=50)
+    description = models.TextField('Descrition')
+
+
+class DiaryEntry(models.Model):
+    """One user's entry"""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
+    emotions = models.ForeignKey(User, verbose_name='user', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, verbose_name='user', on_delete=models.CASCADE)
