08c8a3d930992beae0e3f89666dbc886ceee4bb9
dima_in_the_forest <dmitrykucev@mail.ru>
2023-04-28 15:40:36 +0200

    Removed tests for old functions.

--- app/core/tets/test_admin.py
+++ app/core/tets/test_admin.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-from django.urls import reverse
+# from django.urls import reverse
 from django.test import Client
 
 
@@ -21,10 +21,10 @@ class AdminSiteTests(TestCase):
             password='testpass123',
         )
 
-    def test_users_list(self):
-        """Test that users are listed on page."""
-        url = reverse('admin:core_user_changelist')
-        res = self.client.get(url)
-
-        self.assertContains(res, self.user.name)
-        self.assertContains(res, self.user.email)
+    # def test_users_list(self):
+    #     """Test that users are listed on page."""
+    #     url = reverse('admin:core_user_changelist')
+    #     res = self.client.get(url)
+    #
+    #     self.assertContains(res, self.user.name)
+    #     self.assertContains(res, self.user.email)
