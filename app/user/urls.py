743f55f31efe82403f5b9c65f9fbcb4943752f50
dima_in_the_forest <dmitrykucev@mail.ru>
2023-07-17 12:20:51 +0200

    Added therapist, user, supervisor creation.

--- app/user/urls.py
+++ app/user/urls.py
@@ -7,4 +7,8 @@ app_name = 'user'
 urlpatterns = [
     path('login', views.HomePageView.as_view()),
     # path("<int:pk>/", views.DiaryDetailsView.as_view()),
+    path('register', views.ChooseUserView.as_view(), name="register"),
+    path('register_therapist', views.TherapistCreateView.as_view(), name="register_therapist"),
+    path('register_patient', views.PatientCreateView.as_view(), name="register_patient"),
+    path('register_supervisor', views.SupervisorCreateView.as_view(), name="register_supervisor"),
 ]
